2c09d1fe7fd2b9283857d484c8880b022223d2a0
Kapil Arora <kapilarora5252@gmail.com>
2023-06-15 16:17:18 +0530

    Add files via upload

---
+++ binarySearch2DArraysII.java
@@ -0,0 +1,27 @@
+
+public class binarySearch2DArraysII {
+
+}
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int row = 0;
+        int col = c - 1;
+
+        while((row < r) && (col >= 0)) {
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(matrix[row][col] > target) {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+}
